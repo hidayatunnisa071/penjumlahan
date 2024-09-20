c471dea36658c0de636de6ac047a77945eba1f7a
hidayatunnisa071 <hdayatunnisa071@gmail.com>
2024-09-20 18:13:57 +0800

    Add files via upload

---
+++ InputPenjumlahan.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class InputPenjumlahan {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int angka1, angka2, hasil;
+
+        System.out.println("Bilangan 1:");
+        angka1 = in.nextInt();
+        System.out.println("Bilangan 2:");
+        angka2 = in.nextInt();
+
+        hasil = angka1 + angka2;
+
+
+
+    }
+    
+}
